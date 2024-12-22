5523a57700edeeafa52f968ea8765664a25ab5c2
khanhbuan <21020267@vnu.edu.vn>
2024-12-22 21:24:07 +0700

    add .env.local

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,13 +2,14 @@ import mysql from 'mysql2/promise';
 
 // Cấu hình kết nối database
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'fall2024c56g8_crab',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.DB_POOL_SIZE,
+  queueLimit: process.env.DB_QUEUE_LIMIT,
+  connectTimeout: process.env.DB_TIMEOUT
 };
 
 // Biến lưu trữ connection pool
